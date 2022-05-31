f5393a527b067a9189055b5ecda3061173677311
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2022-05-31 00:05:37 -0300

    Add MainActivity and update AndroidManifest

---
+++ android/src/main/com/example/speech_bubble/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.speech_bubble;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+
+public class MainActivity extends FlutterActivity {
+  // You can keep this empty class or remove it. Plugins on the new embedding
+  // now automatically registers plugins.
+}
